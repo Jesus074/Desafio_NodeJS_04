f742a9be25a92de398dd8051cb40681ba51a91b5
jesus074 <aleflima2314@gmail.com>
2022-03-19 17:29:56 -0300

    Desafio 04 completo ALEF

---
+++ src/classes/Alunos.ts
@@ -0,0 +1,20 @@
+class Alunos {
+	// Proprieties
+	Nome: String;
+	Idade: number;
+	Nota: number;
+
+	// Constructor
+
+	constructor(nome: string, idade: number, nota:number) {
+
+	this.Nome = nome  
+
+	this.Idade = idade
+
+	this.Nota = nota
+	}
+}
+
+
+module.exports = Alunos
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { NOMEM } from "dns";
 
 dotenv.config();
 
@@ -33,11 +34,48 @@ app.use(express.json());
  * Server Activation
  */
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`Listening on port ${PORT}`);
 
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+
+	const readline = require('readline')
+
+	const Alunos = require('./classes/Alunos')
+
+
+
+	const rl = readline.createInterface({
+
+		input: process.stdin,
+		output: process.stdout
+
+	});
+	const question = (str: string) => new Promise((resolve) => rl.question(str, resolve));
+
+	const totalAlunos = Number(await question('Qual a quantidade de alunos ?'))
+
+	let arrayAlunos: Alunos [] = []
+
+	for (var i = 0;
+		i < totalAlunos; i++) {
+
+		const aluno = new Alunos()			
+			aluno.Nome = await question('Qual o nome do aluno ?')
+
+			 aluno.Nota = Number(await question('Qual a nota do aluno ?'))
+
+			 aluno.Idade = Number(await question('Qual a idade do aluno ?'))
+
+		arrayAlunos.push(aluno)
+
+	}
+
+	const sum = arrayAlunos.map(item => item.Nota).reduce((prev, curr) => prev + curr, 0);
+
+	console.log('ALUNOS', arrayAlunos)
+	console.log('Soma das notas', sum)
+
 });
